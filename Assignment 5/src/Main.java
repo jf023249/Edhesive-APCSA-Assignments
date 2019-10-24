08bdec644bb3f89f260c2f87dc69ec409ae47d32
jf023249 <jf023249@Drake-Notebook>
2019-10-23 21:02:06 -0500

    Intermittent project - broken array print

---
+++ Assignment 5/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Main {
+
+	public static void main(String[] args)
+	{
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("How ling do you want the array?");
+		int length = scan.nextInt();
+		
+		if(Math.abs(length) != length)
+		{
+			System.out.println("Not a valid length!");
+		}
+		else
+		{
+			
+		}
+			
+		
+		
+		
+	}
+	
+}
+
+
