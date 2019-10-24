b75f28d362971932d230dde22347e71f1abeee0b
jf023249 <jf023249@Drake-Notebook>
2019-10-23 22:48:22 -0500

    Note: Unfinished Assignment

--- Assignment 5/src/Main.java
+++ Assignment 5/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	public static void main(String[] args)
 	{
 		
-		Scanner scan = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);//
 		System.out.println("How long do you want the array?");
 		int length = scan.nextInt();
 		double sum = 0;
