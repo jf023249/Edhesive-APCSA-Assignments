2f137bd7c956cfd959d77816b6b82592f94fc4bf
jf023249 <jf023249@HBHS-B200-21.sdale.org>
2019-11-07 11:48:24 -0600

    Finished Project

---
+++ Lesson 32 Activity 2/src/Lesson_32_Activity_Two.java
@@ -0,0 +1,62 @@
+
+public class Lesson_32_Activity_Two {
+
+	
+	public static void monthName(int month)
+	{
+		if(month == 1)
+		{
+		System.out.println("31");
+		}
+		if(month == 2)
+		{
+			System.out.println("28");
+		}
+		if(month == 3)
+		{
+			System.out.println("31");
+		}
+		if(month == 4)
+		{
+			System.out.println("30");
+		}
+		if(month == 5)
+		{
+			System.out.println("31");
+		}
+		if(month == 6)
+		{
+			System.out.println("30");
+		}
+		if(month == 7)
+		{
+			System.out.println("31");
+		}
+		if(month == 8)
+		{
+			System.out.println("31");
+		}
+		if(month == 9)
+		{
+			System.out.println("30");
+		}
+		if(month == 10)
+		{
+			System.out.println("31");
+		}
+		if(month == 11)
+		{
+			System.out.println("30");
+		}
+		if(month == 12)
+		{
+			System.out.println("31");
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		
+	}
+	
+}
