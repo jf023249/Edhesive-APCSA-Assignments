771cf67005cdc7943b1f4efada78348f7d95db4f
jf023249 <jf023249@HBHS-B200-21.sdale.org>
2019-11-08 11:45:33 -0600

    Finished Activity

---
+++ Lesson 33 Activity 4/src/Lesson_33_Activity_One.java
@@ -0,0 +1,17 @@
+public class Lesson_33_Activity_One {
+
+	
+	 public static void upper(String a[]) {
+	     for(int i = 0; i < a.length; i++)
+	     {
+	    	 a[i] = a[i].toUpperCase();
+	     }
+	 }
+	
+	
+	public static void main(String[] args)
+	{
+		
+	}
+	
+}
